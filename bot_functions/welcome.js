e8c8a458b46b1ff8cc2e1ef6a8332005b3b021c4
Stephen Burns <burns140@purdue.edu>
2019-12-07 02:38:20 -0500

    missed plus

--- bot_functions/welcome.js
+++ bot_functions/welcome.js
@@ -15,7 +15,7 @@ module.exports.welcome = function(member) {
         `channels have a bit more flexibility when finding a team. ` +
         `The shard-it-keep-it channel uses information gathered by clan member Jiangshi to help you determine whether the ` + 
         `weapon rolls that you've gotten are worth keeping. In the guides section, you can see information that is useful for various activities. ` +
-        `If you would like to be notified when people are searching for certain activities, message the admins to be added as lfg-raider, lfg-pve'er, and/or lfg-pvper. `
+        `If you would like to be notified when people are searching for certain activities, message the admins to be added as lfg-raider, lfg-pve'er, and/or lfg-pvper. ` +
         `Please type '!register' in chat to get set up with Charlemagne. ` +
         `If you visit the know-your-role channel, you can mark which dlc's you have so that others know what activities you can be involved in. ` +
         `We are constantly changing, so if you have any questions, suggestions, or anything else, feel free to contact one of the admins: ` + 
