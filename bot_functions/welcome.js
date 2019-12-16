5dc129948d9a727f7840b28de9df90e44d6a1ca4
Stephen Burns <burns140@purdue.edu>
2019-12-15 18:12:52 -0600

    change bot message for know your role

--- bot_functions/welcome.js
+++ bot_functions/welcome.js
@@ -12,11 +12,11 @@ module.exports.welcome = function(member) {
         `for each activity as well as designated lfg chats. The lfg channels use a bot to create and ` + 
         `schedule activities and can be used when you have a specific activity in mind, while the chat ` + 
         `channels have a bit more flexibility when finding a team. ` +
-        `The shard-it-keep-it channel uses information gathered by clan member Jiangshi to help you determine whether the ` + 
-        `weapon rolls that you've gotten are worth keeping. In the guides section, you can see information that is useful for various activities. ` +
+        `The weapons channel uses information gathered by clan member Jiangshi to help you determine whether the ` + 
+        `weapon rolls you've gotten are worth keeping. In the guides section, you can see information that is useful for various activities. ` +
         `If you would like to be notified when people are searching for certain activities, message the admins to be added as lfg-raider, lfg-pve'er, and/or lfg-pvper. ` +
         `Please type '!register' in chat to get set up with Charlemagne. ` +
-        `If you visit the know-your-role channel, you can mark which dlc's you have so that others know what activities you can be involved in. ` +
+        `Visit the 'know your role' channel to select which classes you main. ` +
         `We are constantly changing, so if you have any questions, suggestions, or anything else, feel free to contact one of the admins: ` + 
         `'Swiftmood', 'The Internet', 'aPhantomDolphin', 'Juncy', 'Azzarie', or 'Mr_Saltshaker1'. ` +
        // `<@360437260333744128>, <@209836090280902656>, <@232691801771868160>, <@147417761725808641>, or <@153392262171066369>.` + 
